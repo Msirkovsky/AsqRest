19dc032f911b6503f9a76a60e2ce225639852cfb
MaraSira <msirkovsky@gmail.com>
2014-08-26 18:57:42 +0200

    Blog entity

---
+++ web/asqRest.js
@@ -0,0 +1,10 @@
+
+function main()
+{
+//alert('Test');
+}
+
+
+main();
+
+
---
+++ web/test.js
@@ -0,0 +1,48 @@
+﻿function main()
+{
+	var div = document.getElementById("viewDiv");
+	div.innerHTML  ="test";   
+
+
+div.innerHTML  = div.innerHTML + "<br/>test GET";   
+$.ajax("api/blog")
+  .done(function(data) {
+    alert( data );
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+
+div.innerHTML  = div.innerHTML + "<br/>test POST";
+$.ajax(
+	{
+		type: "POST",
+		url:"api/blog",
+		contentType: "application/json",
+		data: {"data": "mydata"}
+})
+  .done(function(data) {
+    alert( data );
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+
+div.innerHTML  = div.innerHTML + "<br/>test PUT";
+$.ajax(
+	{
+		type: "PUT",
+		url:"api/blog",
+		contentType: "application/json",
+		data: {"data": "mydata"}
+})
+  .done(function(data) {
+    alert( data );
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+
+//	div.innerHTML  = div.innerHTML + "<br/>test DELETE";
+}
+

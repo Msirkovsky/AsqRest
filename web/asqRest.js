9c052567a0ed5d33a3d056350d70f5ba8fd68a6b
MaraSira <msirkovsky@gmail.com>
2014-08-28 18:00:25 +0200

    Asynquence basic

--- web/asqRest.js
+++ web/asqRest.js
@@ -7,4 +7,39 @@ function main()
 
 main();
 
+//hlavní funkce
+function AsqlRestClient(url) {
 
+    var _url = url;
+    
+    // má to tam být?
+    var genericError =null;
+    function registerGenericError(errorCallback)
+    {
+        genericError = errorCallback;
+    }
+    //???????????????????
+
+    function then(done)
+    {
+
+
+    }
+
+
+    function post(data) {
+        
+       return $.ajax(
+    {
+        type: "POST",
+        url: _url,
+        contentType: "application/json",
+        data:  data
+    })
+    }
+
+    return {
+        post : post
+    };
+
+}
--- web/test.js
+++ web/test.js
@@ -1,48 +1,82 @@
-﻿function main()
-{
-	var div = document.getElementById("viewDiv");
-	div.innerHTML  ="test";   
+﻿function main() {
+    var div = document.getElementById("viewDiv");
+    div.innerHTML = "test";
+
+    div.innerHTML = div.innerHTML + "<br/>test GET";
+
+    function addResult(text)
+    {
+       div.innerHTML = div.innerHTML + "<br/>" + text;
+    }
+
+        function addResultBold(text)
+    {
+       div.innerHTML = div.innerHTML + "<br/><b>" + text + '</b>';
+    }
+
+    var client = new AsqlRestClient('api/blog');
+    var promise = client.post({"data":"test"});
+    promise.done(function (data) {
+        addResultBold("AsqlRestClient - test GET  - ok");
+    }).fail(function () {
+        addResultBold("AsqlRestClient - test GET  - error");
+    });
 
 
-div.innerHTML  = div.innerHTML + "<br/>test GET";   
-$.ajax("api/blog")
-  .done(function(data) {
-    alert( data );
-  })
-  .fail(function() {
-    alert( "error" );
-  });
+    $.ajax("api/blog")
+        .done(function(data) {
+            addResult("test GET - ok");
+        })
+        .fail(function() {
+            addResult("test GET - error");
+        });
+    
 
-div.innerHTML  = div.innerHTML + "<br/>test POST";
-$.ajax(
-	{
-		type: "POST",
-		url:"api/blog",
-		contentType: "application/json",
-		data: {"data": "mydata"}
-})
-  .done(function(data) {
-    alert( data );
-  })
-  .fail(function() {
-    alert( "error" );
-  });
+    $.ajax(
+        {
+            type: "POST",
+            url: "api/blog",
+            contentType: "application/json",
+            data: { "data": "mydata" }
+        })
+        .done(function(data) {
+            addResult("test POST ok");
+        })
+        .fail(function() {
+            addResult("test POST error");
+        });
 
-div.innerHTML  = div.innerHTML + "<br/>test PUT";
-$.ajax(
-	{
-		type: "PUT",
-		url:"api/blog",
-		contentType: "application/json",
-		data: {"data": "mydata"}
-})
-  .done(function(data) {
-    alert( data );
-  })
-  .fail(function() {
-    alert( "error" );
-  });
+    
 
-//	div.innerHTML  = div.innerHTML + "<br/>test DELETE";
+    $.ajax(
+        {
+            type: "PUT",
+            url: "api/blog",
+            contentType: "application/json",
+            data: { "data": "mydata" }
+        })
+        .done(function(data) {
+            addResult("test PUT - ok");
+        })
+        .fail(function() {
+            addResult("test PUT - error");
+        });
+
+    
+
+    $.ajax(
+        {
+            type: "DELETE",
+            url: "api/blog",
+            contentType: "application/json",
+            data: { "data": "mydata" }
+        })
+        .done(function(data) {            
+            addResult("test DELETE - ok");
+        })
+        .fail(function() {
+            addResult("test DELETE - error");
+        });
+
 }
 
